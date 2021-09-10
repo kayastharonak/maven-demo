90defb8ad84e77864644a1e3516980d22f0285bc
ronkayas <ronak.kayastha@publicissapient.com>
2021-09-11 01:17:35 +0530

    updated model

--- src/test/java/com/demo/dao/test/CustomerDAOTest.java
+++ src/test/java/com/demo/dao/test/CustomerDAOTest.java
@@ -10,21 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomerDAOTest {
 
-    private CustomerDAO customerDAO;
+	private CustomerDAO customerDAO;
 
-    private Customer customer;
+	private Customer customer;
 
-    @BeforeEach
-    void beforeEach() {
-        customerDAO = new CustomerDAOImpl();
-        customer = new Customer("1", "Ronak", "Kayastha");
-    }
+	@BeforeEach
+	void beforeEach() {
+		customerDAO = new CustomerDAOImpl();
+		customer = new Customer();
+		customer.setFirstName("Ronak");
+		customer.setId("1");
+		customer.setLastName("Kayastha");
+	}
 
-
-    @Test
-    void testAddCustomer() {
-        customerDAO.addCustomer(customer);
-        assertTrue(customerDAO.getAll().size() > 0);
-    }
+	@Test
+	void testAddCustomer() {
+		customerDAO.addCustomer(customer);
+		assertTrue(customerDAO.getAll().size() > 0);
+	}
 
 }
